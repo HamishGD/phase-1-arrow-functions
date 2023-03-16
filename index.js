88dac7d80b86dc70d964a3ae555a41aa42fad8f0
hamish <hamishgeorgedavie@gmail.com>
2023-03-16 12:57:55 +1000

    Arrow lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {}
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+const square = (parameter1) => parameter1 * parameter1;
+
+const divideDivides = (a, b) => a / b;
+let a = 2000;
+let b = 100;
+
+
+
+
+
+
